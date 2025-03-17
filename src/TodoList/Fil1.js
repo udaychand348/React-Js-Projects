96d8f1e53dc75c35607f99aa6009e0c5ba620809
Choulla Udaychand <udaychandchoulla@gmail.com>
2025-03-17 20:08:42 +0530

    added h2 tag

--- src/TodoList/Fil1.js
+++ src/TodoList/Fil1.js
@@ -31,6 +31,7 @@ const Fil1 = () => {
     <div>
       <center>
         <h1> Todo List Management Application</h1>
+        <h2> It is an Application used for to remember daily tasks</h2>
         <Display task={task} deletehandler={deletehandler} />
         <form onSubmit={shandler} >
             <input type="text" placeholder="Enter your activity" name="todo" value={todo} onChange={chandler} />&nbsp;&nbsp;
